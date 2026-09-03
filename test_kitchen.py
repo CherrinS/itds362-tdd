4ad3351a7ff97990be2172e0bedc12575a8d423f
CherrinS <arin.jiranan@gmail.com>
2026-09-03 12:23:14 +0700

    Step5.1

--- test_kitchen.py
+++ test_kitchen.py
@@ -11,18 +11,22 @@ from kitchen import Quantity
  
  
 def test_multiplication():
-    flour = Quantity(200)
+    flour = Quantity(200,1)
     flour.times(3)
     assert flour.times(3).amount == 600
 
 def test_multiplication_by_two():
-    flour = Quantity(200)
+    flour = Quantity(200,1)
     flour.times(2)
     assert flour.times(2).amount == 400
 
 def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
+    assert Quantity(200,1) == Quantity(200,1)
+    assert Quantity(200,1) != Quantity(300,1)
+
+def test_grams_are_not_ounces():
+    assert Quantity(1, "g") != Quantity(1, "oz")
+
 
 
 
