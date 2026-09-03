51e778f65500a0ed0479f094247f5d06a0045001
CherrinS <arin.jiranan@gmail.com>
2026-09-03 12:16:51 +0700

    Step3.1

--- kitchen.py
+++ kitchen.py
@@ -2,32 +2,11 @@
 class Quantity:
     def __init__(self, amount):
         self.amount = amount
- 
-    def times(self, multiplier):
-        self.amount = 600
-
-    def times(self, multiplier):
-        self.amount = self.amount * multiplier
 
     def times(self, multiplier):
         return Quantity(self.amount * multiplier)
 
-    def __eq__(self, other):
-        return self.amount == other.amount
- 
-    def __repr__(self):
-        return f"Quantity({self.amount})"
-    
-class Quantity:
-    def __init__(self, amount, unit):
-        self.amount = amount
-        self.unit = unit
- 
-    def times(self, multiplier):
-        return Quantity(self.amount * multiplier, self.unit)
- 
-    def __eq__(self, other):
-        return self.amount == other.amount and self.unit == other.unit
+
  
 
 
--- test_kitchen.py
+++ test_kitchen.py
@@ -13,24 +13,12 @@ from kitchen import Quantity
 def test_multiplication():
     flour = Quantity(200)
     flour.times(3)
-    assert flour.amount == 600
+    assert flour.times(3).amount == 600
 
 def test_multiplication_by_two():
     flour = Quantity(200)
     flour.times(2)
-    assert flour.amount == 400
-
-def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
-    assert flour.times(3).amount == 600
     assert flour.times(2).amount == 400
-
-def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
-
-def test_grams_are_not_ounces():
-    assert Quantity(1, "g") != Quantity(1, "oz")
 
 
 
