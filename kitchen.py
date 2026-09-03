bd4f9e8064ec3b31136c41f0ca8d4f2c499391cb
CherrinS <arin.jiranan@gmail.com>
2026-09-03 12:08:14 +0700

    Step5

--- kitchen.py
+++ kitchen.py
@@ -17,5 +17,17 @@ class Quantity:
  
     def __repr__(self):
         return f"Quantity({self.amount})"
+    
+class Quantity:
+    def __init__(self, amount, unit):
+        self.amount = amount
+        self.unit = unit
+ 
+    def times(self, multiplier):
+        return Quantity(self.amount * multiplier, self.unit)
+ 
+    def __eq__(self, other):
+        return self.amount == other.amount and self.unit == other.unit
+ 
 
 
